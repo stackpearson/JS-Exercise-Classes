66d44eeac4f45d708bf7ecc904fc6af37df916b7
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-04-30 20:04:24 -0600

    Tasks 3 & 4 complete

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ class Car {
   }
 
   drive(distance, milesPerGallon) {
-    //DTE represents distance till empty
+    //DTE = distance till empty
     let DTE = this.tank * this.milesPerGallon;
 
     if (this.tank > 0) {
@@ -118,7 +118,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(lambdasianObject) {
+    this.name = lambdasianObject.name;
+    this.age = lambdasianObject.age;
+    this.location = lambdasianObject.location;
+  }
 
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -135,7 +143,22 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(instructorObject) {
+    super(instructorObject);
+    this.instructor = instructorObject.instructor;
+    this.specialty = instructorObject.specialty;
+    this.favLanguage = instructorObject.favLanguage;
+    this.catchPhrase = instructorObject.catchPhrase;
+  }
+  
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 
 }
 
@@ -154,7 +177,26 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(studentObject) {
+    super(studentObject);
+    this.student = studentObject.student;
+    this.previousBackground = studentObject.previousBackground;
+    this.className = studentObject.className;
+    this.favSubjects = studentObject.favSubjects;
+  }
+
+  listSubjects() {
+    return this.favSubjects;
+  }
+
+  PRAssignment(subject) {
+   return `${this.name} has submitted a PR for ${this.subject}`;
+  }
+
+  sprintChallenge(subject) {
+   return `${this.name} has begun sprint challenge on ${this.subject}`;
+  }
 
 }
 
